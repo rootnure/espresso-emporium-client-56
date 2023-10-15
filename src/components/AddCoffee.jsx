6a2ada10184dab7f936b1fa99642d98da93459b3
rootnure <nur.diu.2791@gmail.com>
2023-10-16 04:13:31 +0600

    Basic setup (React Router, AddCoffee & UpdateCoffee page)

---
+++ src/components/AddCoffee.jsx
@@ -0,0 +1,11 @@
+
+
+const AddCoffee = () => {
+    return (
+        <div>
+            <h2>Add Coffee</h2>
+        </div>
+    );
+};
+
+export default AddCoffee;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,31 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import './index.css'
+import AddCoffee from './components/AddCoffee.jsx';
+import UpdateCoffee from './components/UpdateCoffee.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/addCoffee",
+    element: <AddCoffee />,
+  },
+  {
+    path: "/updateCoffee",
+    element: <UpdateCoffee />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
