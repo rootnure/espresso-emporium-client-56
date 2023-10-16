b1672e005cdeb6503f1732e19e538f64c48c7f22
rootnure <nur.diu.2791@gmail.com>
2023-10-17 04:24:55 +0600

    Delete single coffee added with delete confirmation sweet alert

--- src/components/PopularProducts/Product.jsx
+++ src/components/PopularProducts/Product.jsx
@@ -1,10 +1,41 @@
 import PropTypes from 'prop-types';
 import { AiFillEye, AiFillEdit, AiTwotoneDelete } from "react-icons/ai";
 import { Link } from 'react-router-dom';
+import Swal from 'sweetalert2';
 
 
 const Product = ({ product }) => {
-    const { photo, name, chef, price } = product;
+    const { _id, photo, name, chef, price } = product;
+
+    const handleDelete = _id => {
+        console.log(_id);
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                fetch(`http://localhost:5000/coffee/${_id}`, {
+                    method: 'DELETE'
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        console.log(data);
+                        if (data.deletedCount > 0) {
+                            Swal.fire(
+                                'Deleted!',
+                                'Coffee has been deleted.',
+                                'success'
+                            )
+                        }
+                    })
+            }
+        })
+    }
 
     return (
         <div className='bg-[#F5F4F188] flex gap-x-6 px-12 py-6 rounded-md'>
@@ -29,13 +60,16 @@ const Product = ({ product }) => {
             </div>
             <div className='flex items-center w-1/12 justify-end'>
                 <div className='flex flex-col gap-y-4 text-white text-xl'>
-                    <Link to='/coffeeDetails'>
+                    <Link to={`/coffeeDetails:${_id}`}>
                         <button className='bg-[#D2B48C] border-2 border-[#D2B48C] hover:text-[#D2B48C] hover:bg-transparent rounded duration-150 p-1.5' title='View Details'><AiFillEye></AiFillEye></button>
                     </Link>
-                    <Link to='/updateExisting'>
+                    <Link to={`/updateExisting/${_id}`}>
                         <button className='bg-[#3C393B] border-2 border-[#3C393B] hover:text-[#3C393B] hover:bg-transparent rounded duration-150 p-1.5' title='Edit/Update Information'><AiFillEdit></AiFillEdit></button>
                     </Link>
-                    <button className='bg-[#EA4744] border-2 border-[#EA4744] hover:text-[#EA4744] hover:bg-transparent rounded duration-150 p-1.5' title='Delete Item'><AiTwotoneDelete></AiTwotoneDelete></button>
+                    <button
+                        onClick={() => handleDelete(_id)}
+                        className='bg-[#EA4744] border-2 border-[#EA4744] hover:text-[#EA4744] hover:bg-transparent rounded duration-150 p-1.5'
+                        title='Delete Item'><AiTwotoneDelete></AiTwotoneDelete></button>
                 </div>
             </div>
         </div>
