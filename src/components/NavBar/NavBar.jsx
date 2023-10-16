4ff91b119e51a43808b7a8e2d4e0ab1ba7c44540
rootnure <nur.diu.2791@gmail.com>
2023-10-17 01:27:31 +0600

    Add new page bg img added and size fixed

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,11 +1,14 @@
+import { Link } from "react-router-dom";
 
 
 const NavBar = () => {
     return (
         <>
-            <nav style={{ backgroundImage: 'url("https://i.ibb.co/v3MpJw2/15.jpg"' }} className="py-6 bg-cover flex gap-6 justify-center items-center fixed top-0 left-0 right-0 z-50">
-                <img src="https://i.ibb.co/jV0kLgs/logo1.png" alt="Logo" className="h-16" />
-                <h1 className="text-5xl font-rancho text-white">Espresso Emporium</h1>
+            <nav style={{ backgroundImage: 'url("https://i.ibb.co/v3MpJw2/15.jpg"' }} className="py-6 bg-cover fixed top-0 left-0 right-0 z-50">
+                <Link to='/' className="flex gap-4 justify-center items-center w-fit mx-auto" title="Espresso Emporium">
+                    <img src="https://i.ibb.co/jV0kLgs/logo1.png" alt="Logo" className="h-16" />
+                    <h1 className="text-5xl font-rancho text-white">Espresso Emporium</h1>
+                </Link>
             </nav>
         </>
     );
--- src/pages/AddNew/AddNew.jsx
+++ src/pages/AddNew/AddNew.jsx
@@ -1,6 +1,7 @@
 import { Helmet } from "react-helmet-async";
 import { MdArrowBack } from "react-icons/md";
 import SectionHeader from "../../components/SectionHeader/SectionHeader";
+import { Link } from "react-router-dom";
 
 
 const AddNew = () => {
@@ -37,40 +38,44 @@ const AddNew = () => {
             <Helmet>
                 <title>Add New Coffee | Espresso Emporium</title>
             </Helmet>
-            <main className="my-28 container mx-auto px-20">
-                <div className="pt-12 pb-6">
-                    <button className="flex items-center gap-x-3 py-4 ps-2 pe-3 rounded-lg hover:bg-[#D2B48C] duration-150 font-rancho text-2xl [text-shadow:_2px_2px_8px_#242222EE]"><MdArrowBack></MdArrowBack> Back to home</button>
-                </div>
-                <div className="bg-[#F4F3F0] rounded p-12">
-                    <div className="text-center my-4">
-                        <SectionHeader
-                            sectionTitle="Add New Coffee" />
-                        <p className="mt-4 px-8 text-lg">Experience the art of coffee at its finest with <span className="font-rancho text-2xl">Espresso Emporium</span>. Journey begins at the world&apos;s finest coffee farms, meticulously brewed by our expert baristas. Explore a world of flavor and passion in every cup.</p>
-                    </div>
-
-                    {/* form */}
-                    <form onSubmit={handleSubmit} className="grid grid-cols-2 gap-6">
-                        {
-                            coffeeFormFields.map(field => <div
-                                key={field.id}
-                                className="space-y-2">
-                                <label
-                                    className="font-bold"
-                                    htmlFor={field.id}>{field.label}</label>
-                                <input
-                                    className="p-2 w-full rounded-md"
-                                    type={field.type}
-                                    name={field.id}
-                                    id={field.id}
-                                    defaultValue={field.defaultValue}
-                                    placeholder={field.placeholder}
-                                    required />
-                            </div>)
-                        }
-                        <div className="col-span-2">
-                            <button className="font-rancho text-2xl text-center w-full border-2 border-[#331A15] py-2 rounded-md bg-[#D2B48C] hover:bg-[#D2B48CDD] duration-75 text-[#331A15]" type="submit">Add Coffee</button>
+            <main className="my-28 bg-cover" style={{ backgroundImage: 'url("https://i.ibb.co/hB6Tp24/11.png")' }}>
+                <div className="container mx-auto px-20">
+                    <section className="pt-12 pb-6">
+                        <Link to="/">
+                            <button className="flex items-center gap-x-3 py-4 ps-2 pe-3 rounded-lg hover:bg-[#D2B48C] duration-150 font-rancho text-2xl [text-shadow:_2px_2px_8px_#242222EE]"><MdArrowBack></MdArrowBack> Back to home</button>
+                        </Link>
+                    </section>
+                    <section className="bg-[#F4F3F0] rounded p-12">
+                        <div className="text-center my-4">
+                            <SectionHeader
+                                sectionTitle="Add New Coffee" />
+                            <p className="mt-4 px-8 text-lg">Experience the art of coffee at its finest with <span className="font-rancho text-2xl">Espresso Emporium</span>. Journey begins at the world&apos;s finest coffee farms, meticulously brewed by our expert baristas. Explore a world of flavor and passion in every cup.</p>
                         </div>
-                    </form>
+
+                        {/* form */}
+                        <form onSubmit={handleSubmit} className="grid grid-cols-2 gap-6">
+                            {
+                                coffeeFormFields.map(field => <div
+                                    key={field.id}
+                                    className="space-y-2">
+                                    <label
+                                        className="font-bold"
+                                        htmlFor={field.id}>{field.label}</label>
+                                    <input
+                                        className="p-2 w-full rounded-md"
+                                        type={field.type}
+                                        name={field.id}
+                                        id={field.id}
+                                        defaultValue={field.defaultValue}
+                                        placeholder={field.placeholder}
+                                        required />
+                                </div>)
+                            }
+                            <div className="col-span-2">
+                                <button className="font-rancho text-2xl text-center w-full border-2 border-[#331A15] py-2 rounded-md bg-[#D2B48C] hover:bg-[#D2B48CDD] duration-75 text-[#331A15]" type="submit">Add Coffee</button>
+                            </div>
+                        </form>
+                    </section>
                 </div>
             </main>
         </>
